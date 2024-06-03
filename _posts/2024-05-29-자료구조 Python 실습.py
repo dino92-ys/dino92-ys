2f5c2d14e1f03e4e5b4613ac6b4edee0e0f361db
dino92-ys <dldudtjs96@naver.com>
2024-06-03 11:14:41 +0900

    용어 정리 위치 변경 등 내용 추가

--- _posts/2024-05-29-자료구조 Python 실습.py
+++ _posts/2024-05-29-자료구조 Python 실습.py
@@ -1,1 +1,19 @@
-print("Hello, world")
+print("I love u");
+
+# daum = 89000
+# naver = 751000
+# 주식 = daum*100 + naver*20
+# print(주식)
+
+# 주식 = (daum*100)*0.05 + (naver*20)*0.1
+# print(주식)
+
+F = 50
+C = (F-32)/1.8
+print(C)
+
+# gcd(a,b): 
+# while a != 0 and b !=0:
+#        if a>b : a=a-b
+#        else : b=b-a
+# return a+b
